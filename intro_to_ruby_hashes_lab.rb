b845da3133919a1f2a7e8f3245ea52d8c42711f2
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 23:30:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,10 @@ monopoly = {
     :shortline => {}
   },
   :rent_in_dollars => {
-    
+    :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
   }
   }
 }
